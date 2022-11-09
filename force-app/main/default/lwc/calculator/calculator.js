0b64e95e3cda7b55ba14e5836f6c640d0e6ad17f
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-11-09 18:52:46 -0500

    Added the functinality to calculate the tip.

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -2,12 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class Calculator extends LightningElement {
   totalBill = 0;
-  service = 'good';
+  service = '';
   numberOfPeople = 1;
   tip = 0;
 
   billAmountHandler(event) {
     this.totalBill = event.target.value;
+  }
+
+  peopleHandler(event) {
+    this.numberOfPeople = event.target.value;
   }
 
   get pickList() {
@@ -20,6 +24,24 @@ export default class Calculator extends LightningElement {
 
 handleChange(event) {
   this.service = event.detail.value;
+}
+
+handleCalculate() {
+  let total = 0;
+  let totalbill = this.totalBill;
+  let service = this.service;
+  let num = this.numberOfPeople;
+
+  if(service === 'good') {
+      total = (totalbill *  .10) / num;
+  } else if (service === 'excellent') {
+    total = (totalbill *  .20) / num;
+  }
+  else if (service === 'bad') {
+    total = (totalbill *  .05) / num;
+  }
+
+  this.tip = total;
 }
 
 }
